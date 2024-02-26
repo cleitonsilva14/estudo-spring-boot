f6ae52ed81007155f822638ed6b06df432eb2705
cleitonsilva14 <cleiton18ads@gmail.com>
2024-02-26 16:19:15 -0300

    Adicionando método getOneProduct na classe ProductController.java

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -1,12 +1,17 @@
 package com.example.springboot.controllers;
 
+import java.lang.StackWalker.Option;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +40,16 @@ public class ProductController {
 	public ResponseEntity<List<ProductModel>> getAllProducts(){
 		return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
 	}
+	
+	@GetMapping("/products/{id}")
+	public ResponseEntity<Object> getOneProduct(@PathVariable(value="id") UUID id){
+		Optional<ProductModel> productO = productRepository.findById(id);
+		if(productO.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(productO.get());
+	}
+	
+	
 	
 }
